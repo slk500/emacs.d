2a42c676b281c066286b2a55de25d88de5fc93fd
Sławomir Grochowski <slawomir.grochowski@exorigo-upos.pl>
2018-12-14 18:05:53 +0100

    hungry-delete

--- init.el
+++ init.el
@@ -15,8 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (flycheck-phpstan htmlize php-mode flycheck which-key use-package try tabbar org-bullets lorem-ipsum counsel company color-theme))))
+   '(hungry-delete flycheck-phpstan htmlize php-mode flycheck which-key use-package try tabbar org-bullets lorem-ipsum counsel company color-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
